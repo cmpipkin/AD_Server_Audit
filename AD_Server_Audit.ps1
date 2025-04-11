19c144eedbb93fe44151b58e0fcc366722a506ed
C M Pipkin <droidclone@gmail.com>
2025-04-11 15:09:19 -0400

    Create AD_Server_Audit.ps1

---
+++ AD_Server_Audit.ps1
@@ -0,0 +1,95 @@
+# AD Server Audit
+<#
+The object is to generate an audit of servers in AD. This script does an ICMP 
+and WinRM service test to see if the server is 'alive'. It gets the creation 
+date and last change date; useful to know if the server needs to be removed 
+from AD.
+#>
+
+param (
+    [Parameter()]
+    [string]$ExcelFile = "$([environment]::getfolderpath("mydocuments"))\AD Server Audit.xlsx"
+
+)
+
+# variables
+$serverListHash = @()
+$serverHash = [ordered]@{}
+
+# query AD servers and get all properties
+$serversAD = Get-ADComputer -Filter "OperatingSystem -like '*Server*'" -Properties *
+
+<#
+itterate through serversAD
+add wanted properties to hash table
+add hash table to list
+#>
+foreach ($server in $serversAD) {
+    # server name
+    $serverHash.Add("Server", "$($server.Name)")
+    # type of OS
+    $serverHash.Add("OS", "$($server.OperatingSystem)")
+    # directory tree path
+    $serverHash.Add("AD Tree", "$($server.CanonicalName)")
+    # creation date
+    $serverHash.Add("Created", "$($server.whenCreated)")
+    # last modified
+    $serverHash.Add("Last Changed", "$($server.whenChanged)")
+
+    # ICMP
+    try {
+        # run connection test
+        $connTest = Test-Connection -ComputerName $server.Name -ErrorAction Stop -Count 1
+    } catch {
+        $connTest = $_
+    } finally {
+        # add up down status to hash
+        if (!$connTest.Exception.Message) {
+            $serverHash.Add("ICMP","up")
+        } else {
+            $serverHash.Add("ICMP","down")
+        }
+    }
+
+    # WinRM
+    try {
+        # run WinRM test
+        $wsmanTest = Test-WSMan -ComputerName $server.Name -ErrorAction Stop
+    } catch {
+        $wsmanTest = $_
+    } finally {
+        # add up down status to hash, if successful gather server information
+        if (!$wsmanTest.Exception.Message) {
+            $serverHash.Add("WinRM","up")
+            # gather server information
+            try {
+                # gather system information
+                $cimData = Get-CimInstance -ClassName CIM_ComputerSystem -ComputerName $server.Name -ErrorAction Stop
+            } catch {
+                $cimData = $_
+            } finally {
+                # add physical and logical cpu count, add hardware model
+                if (!$cimData.Exception.Message) {
+                    $serverHash.add("Physical Proc Count", "$($cimData.NumberOfProcessors)")
+                    $serverHash.add("Logical Proc Count", "$($cimData.NumberOfLogicalProcessors)")
+                    $serverHash.add("Model", "$($cimData.Model)")
+                } else {
+                    # give error code for failure reason
+                    $serverHash.add("WinRM Error", "$($cimData.Exception.Message)")
+                }
+            }
+        } else {
+            $serverHash.Add("WinRM","down")
+        }
+    }
+
+
+    # add hash table to server list
+    $serverListHash += [pscustomobject]$serverHash
+
+    # Rest serverHash var for next itteration
+    $serverHash = [ordered]@{}
+}
+
+# export data to excel
+$serverListHash | Export-Excel -Path $ExcelFile -FreezeTopRow -WorksheetName "AD Server Audit"
